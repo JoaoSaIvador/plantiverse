5769618d83844ca4fb7fc2fbda6936489e697cd8
JoaoSalvador <jpvsalvador05@gmail.com>
2022-09-14 23:28:50 +0100

    Express server setup

---
+++ server/server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const fileUpload = require('express-fileupload');
+require('dotenv').config({ path: "./.env" });
+
+const app = express();
+app.use(express.json())
+app.use(cookieParser());
+app.use(cors({ credentials: true, origin: 'http://localhost:8080' }));
+app.use(express.urlencoded({ extended: false }));
+app.use(fileUpload({ useTempFiles: true }));
+
+// Connect to DB
+mongoose.connect(process.env.DB_CONNECTION, {
+    useNewUrlParser: true,
+}, err => {
+    if (err) {
+        throw err;
+    }
+    console.log('Mongo DB Connection successful!');
+});
+
+// Routes
+app.get('/', (req, res) => {
+    res.json({ msg: 'We are on home' });
+})
+
+// Start listening to the server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log('Server is running on port ', PORT)
+});
