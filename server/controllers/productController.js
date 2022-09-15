01eb4a377f2e9b2aa7f77bee9d53bd8e2ac66b3e
JoaoSalvador <jpvsalvador05@gmail.com>
2022-09-15 17:21:05 +0100

    Product filters, sorting and pagination

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -1,10 +1,60 @@
 const Product = require('../models/productModel');
 
+class APIFeatures {
+    constructor(query, queryString) {
+        this.query = query;
+        this.queryString = queryString;
+    }
+
+    filtering() {
+        const queryObj = { ...this.queryString }
+
+        const excludedFields = ['page', 'sort', 'limit']
+        excludedFields.forEach(el => delete (queryObj[el]))
+
+        // gte = greater than or equal
+        // lte = lesser than or equal
+        // lt = lesser than
+        // gt = greater than
+        let queryStr = JSON.stringify(queryObj)
+        queryStr = queryStr.replace(/\b(gte|gt|lt|lte|regex)\b/g, match => '$' + match)
+
+        this.query.find(JSON.parse(queryStr))
+
+        return this;
+    }
+
+    sorting() {
+        if (this.queryString.sort) {
+            const sortBy = this.queryString.sort.split(',').join(' ')
+            this.query = this.query.sort(sortBy)
+        } else {
+            this.query = this.query.sort('-createdAt')
+        }
+
+        return this;
+    }
+
+    paginating() {
+        const page = this.queryString.page * 1 || 1
+        const limit = this.queryString.limit * 1 || 9
+        const skip = (page - 1) * limit;
+        this.query = this.query.skip(skip).limit(limit)
+        return this;
+    }
+}
+
 const productController = {
     getProducts: async (req, res) => {
         try {
-            // Find all products
-            const products = await Product.find();
+            const features = new APIFeatures(Product.find(), req.query).filtering().sorting().paginating()
+            const products = await features.query;
+
+            res.json({
+                status: 'success',
+                result: products.length,
+                products: products
+            })
 
             return res.json(products);
         } catch (err) {
