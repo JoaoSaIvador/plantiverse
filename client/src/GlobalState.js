5c99fb132810a35ddea5f2bfd9bdd84c4cc4d17e
JoaoSalvador <jpvsalvador05@gmail.com>
2022-09-21 17:19:29 +0100

    Loading page animation and Login/Register pages

--- client/src/GlobalState.js
+++ client/src/GlobalState.js
@@ -1,10 +1,25 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useEffect, useState } from "react";
 import ProductsAPI from './api/ProductsAPI';
+import axios from "axios";
 
 export const GlobalState = createContext();
 
 export const DataProvider = ({ children }) => {
     const [token, setToken] = useState(false);
+
+    const refreshToken = async () => {
+        const res = await axios.get('http://localhost:5000/users/refreshToken');
+
+        setToken(res.data.accessToken);
+    }
+
+    useEffect(() => {
+        const firstLogin = localStorage.getItem('firstLogin');
+
+        if (firstLogin) {
+            refreshToken();
+        }
+    }, []);
 
     const state = {
         token: [token, setToken],
